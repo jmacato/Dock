589f8afa3561844a42348cd6afecf450784bec7b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-10-15 23:37:29 +0200

    Update FileView.xaml.cs

--- samples/Notepad/Views/Documents/FileView.xaml.cs
+++ samples/Notepad/Views/Documents/FileView.xaml.cs
@@ -1,13 +1,19 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using AvaloniaEdit;
 
 namespace Notepad.Views.Documents
 {
     public class FileView : UserControl
     {
+        private readonly TextEditor _textEditor;
+
         public FileView()
         {
             this.InitializeComponent();
+            _textEditor = this.FindControl<TextEditor>("Editor");
+            _textEditor.ShowLineNumbers = true;
         }
 
         private void InitializeComponent()
